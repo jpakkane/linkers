60aa7d36fd7eb11e4000f2316296207f55cf9637
Jussi Pakkanen <jpakkane@gmail.com>
2016-08-14 21:23:13 +0300

    Use fpic.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc -I. -Wall -Wextra -shared b.c -o libb.so -Wl,--no-undefined
-gcc -I. -Wall -Wextra -shared a.c -o liba.so -Wl,--no-undefined -L. -lb
+gcc -I. -Wall -Wextra -fPIC -shared b.c -o libb.so -Wl,--no-undefined
+gcc -I. -Wall -Wextra -fPIC -shared a.c -o liba.so -Wl,--no-undefined -L. -lb
 gcc -I. -Wall -Wextra prog.c -o prog -Wl,--no-undefined -L. -la
