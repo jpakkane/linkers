f393e21ec2efed40b2a58f7ae65c6ce44c244a6b
Jussi Pakkanen <jpakkane@gmail.com>
2016-08-15 23:48:59 +0300

    Uncomment call to dependent library.

--- sysdep/gtkusage.c
+++ sysdep/gtkusage.c
@@ -3,6 +3,7 @@
 int main(int argc, char **argv) {
   GtkApplication *app;
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_object_unref(app);
+  /* Uncomment to add a symbol in a dependent dso. */
+  /*  g_object_unref(app); */
   return 0;
 }
